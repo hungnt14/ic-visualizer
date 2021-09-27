2755d659e06e88b169005c27e8787095bd043566
Nguyen Tien Hung <33962566+hungnt14@users.noreply.github.com>
2021-09-27 16:47:54 +0700

    bug fixed

--- routes/getImage.js
+++ routes/getImage.js
@@ -7,12 +7,12 @@ router.get('/', function(req, res, next) {
       var imgList = JSON.parse(fs.readFileSync("./public/results.json"));
       for (var i = 0; i < imgList.length; i++) {
         if (imgList[i]['id'] == req.query.filename) {
-            if (req.query.move="prev") {
+            if (req.query.move == "prev") {
                 if (i == 0) i = imgList.length - 1;
                 else i--;
                 res.json({id: imgList[i]['id']});
             }
-            if (req.query.move="next") {
+            if (req.query.move == "next") {
                 if (i == imgList.length - 1) i = 0;
                 else i++;
                 res.json({id: imgList[i]['id']});
